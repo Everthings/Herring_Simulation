75a615ce883a58116ffcc1aae424463d43b94ecd
Andy Xu <andyxu12@gmail.com>
2018-07-26 20:44:30 -0400

    -Added herring movement down river

---
+++ Herring_Game/Assets/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position + new Vector3(0, 3, 6);
+    }
+}
