c3888d5b4666aef91a63fc82c8e97f94fef28e77
Andy Xu <andyxu12@gmail.com>
2018-07-27 10:21:59 -0400

    -Added better zoom

--- Herring_Game/Assets/CameraScript.cs
+++ Herring_Game/Assets/CameraScript.cs
@@ -7,11 +7,14 @@ public class CameraScript : MonoBehaviour
 
     public int maxZ;
     public int minZ;
-    public int speed;
+    public int ScrollSpeed;
+    public int ZoomSpeed;
+
+    Vector3 startPos;
 
     void Start()
     {
-
+        startPos = transform.position;
     }
 
     // Update is called once per frame
@@ -19,26 +22,43 @@ public class CameraScript : MonoBehaviour
     {
         if (Input.GetKey("up") || Input.GetKey("w"))
         {
-            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, ScrollSpeed * Time.deltaTime, 0));
 
             if (transform.position.z < minZ)
                 transform.position = new Vector3(transform.position.x, transform.position.y, minZ);
         }
         else if (Input.GetKey("down") || Input.GetKey("s"))
         {
-            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, -ScrollSpeed * Time.deltaTime, 0));
 
             if (transform.position.z > maxZ)
                 transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward (against z direction... weird, I know)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));   
+
+            if (transform.position.y > 40)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                Vector3 dir = ray.direction.normalized;
+                transform.Translate(new Vector3(dir.x, dir.z, dir.y) * -ZoomSpeed);
+
+            }
+
+            if (transform.position.z < 0)
+                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            else if (transform.position.z > 1900)
+                transform.position = new Vector3(transform.position.x, transform.position.y, 1900);
         }
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backward (with z direction)
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+            if (transform.position.y < startPos.y)
+            {
+                Vector3 dir = new Vector3(transform.position.x - startPos.x, transform.position.y - startPos.y, 0).normalized;
+                transform.Translate(new Vector3(dir.x, 0, dir.y) * ZoomSpeed);
+            }
         }
     }
 }
