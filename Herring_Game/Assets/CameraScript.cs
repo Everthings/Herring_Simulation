d64862e8deb01976212a0e7bc825461a7449889c
Andy Xu <andyxu12@gmail.com>
2018-07-26 21:12:32 -0400

    -Added zoom functionality

--- Herring_Game/Assets/CameraScript.cs
+++ Herring_Game/Assets/CameraScript.cs
@@ -17,19 +17,28 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward (against z direction... weird, I know)
+        if (Input.GetKey("up") || Input.GetKey("w"))
         {
             transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
 
             if (transform.position.z < minZ)
                 transform.position = new Vector3(transform.position.x, transform.position.y, minZ);
         }
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backward (with z direction)
+        else if (Input.GetKey("down") || Input.GetKey("s"))
         {
             transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
 
             if (transform.position.z > maxZ)
                 transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward (against z direction... weird, I know)
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));   
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backward (with z direction)
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
         }
     }
 }
