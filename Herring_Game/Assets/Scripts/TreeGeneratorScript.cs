510c5dd21e28989ab2bb238523e5fbf813fea06a
Andy Xu <andyxu12@gmail.com>
2018-07-29 16:35:05 -0400

    -Revised tree generator script

--- Herring_Game/Assets/Scripts/TreeGeneratorScript.cs
+++ Herring_Game/Assets/Scripts/TreeGeneratorScript.cs
@@ -6,10 +6,22 @@ public class TreeGeneratorScript : MonoBehaviour
 {
 
     public Terrain river;
-    public Tree prefab;
+    public Tree age1;
+    public Tree age2;
+    public Tree age3;
+    public Tree age4;
+    public Tree age5;
+    public Tree age6;
+    public Tree age7;
+    public Tree age8;
+    public Tree age9;
+    public Tree age10;
 
     List<Vector3> positions = new List<Vector3>();
     List<Vector2> possibleLocations = new List<Vector2>();
+    List<TreeData> prefabs = new List<TreeData>();
+
+    List<Tree> agesInOrder = new List<Tree>();
 
     public float upper_bound;
     public float lower_bound;
@@ -20,11 +32,20 @@ public class TreeGeneratorScript : MonoBehaviour
     public bool culvert_removed = false;
     public float survival_rate;
 
-    float[,] heightMap;
-
     // Use this for initialization
     void Start()
     {
+        agesInOrder.Add(age1);
+        agesInOrder.Add(age2);
+        agesInOrder.Add(age3);
+        agesInOrder.Add(age4);
+        agesInOrder.Add(age5);
+        agesInOrder.Add(age6);
+        agesInOrder.Add(age7);
+        agesInOrder.Add(age8);
+        agesInOrder.Add(age9);
+        agesInOrder.Add(age10);
+
         populatePossibleLocations();
         addTrees(5);
         drawTrees();
@@ -33,12 +54,21 @@ public class TreeGeneratorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            int length = positions.Count;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                swapTreeType(i);
+            }
+                
 
+            drawTrees();
+        }
     }
 
     void populatePossibleLocations()
     {
-
         for(int i = (int)right_bound; i < left_bound; i+=5)
         {
             for (int j = (int)lower_bound; j < upper_bound; j += 5)
@@ -58,13 +88,71 @@ public class TreeGeneratorScript : MonoBehaviour
             Vector2 randPos = possibleLocations[(int)(Random.Range(0, 0.999f) * possibleLocations.Count)];
             Vector3 pos = new Vector3(randPos.x, 7, randPos.y);
             positions.Add(pos);
+            prefabs.Add(new TreeData(0, null));
 
             possibleLocations.Remove(randPos);
         }
     }
 
+    void addTree(Vector2 newPos, int newAge)
+    {
+        Vector3 pos = new Vector3(newPos.x, 7, newPos.y);
+        positions.Add(pos);
+        prefabs.Add(new TreeData(newAge, null));
+    }
+
+    void swapTreeType(int index)
+    {
+        addTree(new Vector2(positions[index].x, positions[index].z), (prefabs[index].getAge()+1)%10);
+        destroyTree(index);
+
+        positions.Remove(positions[index]);
+        prefabs.Remove(prefabs[index]);
+    }
+
     void drawTrees(){
         for (int i = 0; i < positions.Count; i++)
-            Instantiate(prefab, positions[i], Quaternion.identity);
+            prefabs[i].setTreePrefab(Instantiate(agesInOrder[prefabs[i].getAge()], positions[i], Quaternion.identity));
+    }
+
+    void destroyTree(int index)
+    {
+        if (prefabs[index].getPrefab() != null)
+        {
+            Destroy(prefabs[index].getPrefab());
+        }
+    }
+
+    public List<Vector3> getTreePositions()
+    {
+        return positions;
+    }
+}
+
+class TreeData
+{
+
+    int age;
+    Tree prefab;
+
+    public TreeData(int age, Tree prefab)
+    {
+        this.age = age;
+        this.prefab = prefab;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public Tree getPrefab()
+    {
+        return prefab;
+    }
+
+    public void setTreePrefab(Tree t)
+    {
+        this.prefab = t;
     }
 }
---
+++ Herring_Game/Assets/TreeShrubCollectionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeShrubCollectionScript : MonoBehaviour
+{
+
+    List<List<Vector3>> sectionPositions = new List<List<Vector3>>();
+
+    // Use this for initialization
+    void Start () {
+        sectionPositions.Add(transform.Find("Section1").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section2").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section3").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section4").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section5").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section6").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section7").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section8").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section9").GetComponent<TreeGeneratorScript>().getTreePositions());
+        sectionPositions.Add(transform.Find("Section10").GetComponent<TreeGeneratorScript>().getTreePositions());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
