2e153fce41d2565fb2bdc144b8dfb896c0c69059
Andy Xu <andyxu12@gmail.com>
2018-07-30 15:58:31 -0400

    -Fixed tree/shrub generation

--- Herring_Game/Assets/Scripts/TreeShrubGeneratorScript.cs
+++ Herring_Game/Assets/Scripts/TreeShrubGeneratorScript.cs
@@ -6,6 +6,17 @@ public class TreeShrubGeneratorScript : MonoBehaviour
 {
 
     public Terrain river;
+    public Terrain river1;
+    public Terrain river2;
+    public Terrain river3;
+    public Terrain river4;
+    public Terrain river5;
+    public Terrain river6;
+    public Terrain river7;
+    public Terrain river8;
+    public Terrain river9;
+    public Terrain river10;
+
     public Tree age1;
     public Tree age2;
     public Tree age3;
@@ -58,6 +69,24 @@ public class TreeShrubGeneratorScript : MonoBehaviour
       
     }
 
+    void populatePossibleLocations()
+    {
+        // set array of all positions on land (increments by 5 cuz 2 trees 1 apart is too much overlap)
+
+        for(int i = (int)right_bound; i < left_bound; i+=5)
+        {
+            for (int j = (int)lower_bound; j < upper_bound; j += 5)
+            {
+                if (river.SampleHeight(new Vector3(i, 0, j)) >= 6.9f && river1.SampleHeight(new Vector3(i, 0, j)) >= 6.9f
+                    && river2.SampleHeight(new Vector3(i, 0, j)) >= 6.9f && river3.SampleHeight(new Vector3(i, 0, j)) >= 6.9f
+                    && river4.SampleHeight(new Vector3(i, 0, j)) >= 6.9f)
+                {
+                    possibleLocations.Add(new Vector2(i, j));
+                } 
+            }
+        }
+    }
+
     public void incrementTreeAges()
     {
         int length = treePositions.Count;
@@ -67,22 +96,6 @@ public class TreeShrubGeneratorScript : MonoBehaviour
         }
 
         drawTrees();
-    }
-
-    void populatePossibleLocations()
-    {
-        // set array of all positions on land (increments by 5 cuz 2 trees 1 apart is too much overlap)
-
-        for(int i = (int)right_bound; i < left_bound; i+=5)
-        {
-            for (int j = (int)lower_bound; j < upper_bound; j += 5)
-            {
-                if (river.SampleHeight(new Vector3(i, 0, j)) >= 6.9f)
-                {
-                    possibleLocations.Add(new Vector2(i, j));
-                } 
-            }
-        }
     }
 
     public void addTrees(int num)
