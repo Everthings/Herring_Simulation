edaedc97dcf65fe6714a1966b5609104d0ae61ca
Nate fay <nate.c.fay@gmail.com>
2018-08-03 12:18:23 -0400

    started popups

---
+++ Herring_Game/Assets/Scripts/OKButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OKButton : MonoBehaviour {
+
+    public string name;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void clicked(){
+        GameObject.Find("Canvas").transform.Find(name).gameObject.SetActive(false);
+    }
+
+}
