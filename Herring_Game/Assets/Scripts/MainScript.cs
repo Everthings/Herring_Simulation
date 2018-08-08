171b89d57950ced8135057e1b993c8a6867310cd
Andy Xu <andyxu12@gmail.com>
2018-08-08 15:47:58 -0400

    -Restored good version

--- Herring_Game/Assets/Scripts/MainScript.cs
+++ Herring_Game/Assets/Scripts/MainScript.cs
@@ -18,8 +18,6 @@ public class MainScript : MonoBehaviour {
     public float climateMultiplier = 0.1f;
     public float fishingMultiplier = 0.2f;
 
-    int lol;
-
     // GAME DESIGN
     /*
      * Year 0: Herring spawn and run down river
